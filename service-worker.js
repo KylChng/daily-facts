85c26e8cd5862edc294d0dd36ae9291f466da790
KylChng <kclingyi@gmail.com>
2026-09-04 11:14:33 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-facts-v3';
+const CACHE = 'daily-facts-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
